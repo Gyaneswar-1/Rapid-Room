a63b2b0cbd012c5fadf0a97f57ab4166138a9c15
Bibek-only <bibekbibek966@gmail.com>
2025-01-17 21:12:33 +0530

    added the common folder for signup/in data validation

---
+++ common/src/index.ts
@@ -0,0 +1,4 @@
+import { SignupSchema, SignupType } from "./schema/signupSchema.js";
+import { SigninSchema, SigninType } from "./schema/signinSchema.js";
+
+export { SignupSchema, SignupType, SigninSchema, SigninType };
