3b1833fe3c9f788b3015cd0a54be75381c9032d3
Bibek-only <bibekbibek966@gmail.com>
2025-01-22 20:34:31 +0530

    setup the nodemailer for sending email

---
+++ server/src/helper/SendEmail.helper.ts
@@ -0,0 +1,17 @@
+import { transporter } from "../utils/NodemailerSetup.js";
+
+type sendEamilType = {
+    to: string;
+    subject: string;
+    text: string;
+};
+export const sendEmail = (obj: sendEamilType) => {
+    const mailOptions = {
+        from: process.env.GOOGLE_EMAIL,
+        to: obj.to,
+        subject: obj.subject,
+        text: obj.text,
+    };
+
+    return transporter.sendMail(mailOptions);
+};
