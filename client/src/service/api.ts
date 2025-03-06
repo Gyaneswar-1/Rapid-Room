4d0b4fa16a5b5692265b66e875c5fd9a62711edb
Gyaneswar-1 <gyaneswarrout12345@gmail.com>
2025-03-06 17:45:12 +0530

    update API endpoint configuration for local and deployed environments; enhance CORS settings for production

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -1,3 +1,7 @@
-// const API = "http://localhost:3000/api/v1";
-const API = "https://rapid-room-server.onrender.com";
+const API =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000/api/v1" // Local API
+    : "https://rapid-room-server.onrender.com/api/v1"; // Deployed API
+
 export default API;
+
